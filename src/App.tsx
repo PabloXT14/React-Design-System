6b3167a9e2943769768f9512864c7e3958523f2a
Pablo Alan <pabloxt14@gmail.com>
2022-10-12 22:30:45 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles/global.css"
+
+export function App() {
+  return (
+    <div className="App">
+      <button
+        className="font-bold bg-violet-500 text-white m-4 px-2 py-2 rounded hover:bg-violet-400">
+        Click me
+      </button>
+    </div>
+  )
+}
